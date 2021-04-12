67fce20ddb8e160c28bcf0068194e62a75f029f8
pengcan <admin123>
2021-04-12 10:28:38 +0800

    Initial commit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @program: helloWord
+ * @description: 新建一个helloWord文件
+ * @author: pengcan
+ * @create: 2021-04-12 10:23
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("helloWord");
+    }
+}
